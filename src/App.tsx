e2c0fe5b8f4da2a3becc902a4547c850480054f2
Lelum Akalanka <akalanka12.me@gmail.com>
2025-11-26 16:19:07 +0530

    errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { OperationsProvider } from './context/OperationsContext';
 import { ScrollToTop } from './components/ScrollToTop';
-import { ProtectedRoute } from './components/ProtectedRoute';
 // Operations Routes
 import { LoginPage as OperationsLoginPage } from './pages/LoginPage';
 import { DashboardPage } from './pages/DashboardPage';
@@ -26,38 +25,36 @@ export function App() {
         <Route path="/operations/login" element={<OperationsProvider>
               <OperationsLoginPage />
             </OperationsProvider>} />
-        <Route path="/operations/dashboard" element={<ProtectedRoute><OperationsProvider>
+        <Route path="/operations/dashboard" element={<OperationsProvider>
               <DashboardPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/user-management" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/user-management" element={<OperationsProvider>
               <UserManagementPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/student/:id" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/student/:id" element={<OperationsProvider>
               <StudentDetailPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/edit-user/:id" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/edit-user/:id" element={<OperationsProvider>
               <EditUserPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/lecturer/:id" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/lecturer/:id" element={<OperationsProvider>
               <LecturerDetailPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/courses" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/courses" element={<OperationsProvider>
               <CourseManagementPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/payments" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/payments" element={<OperationsProvider>
               <PaymentsPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/announcements" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/announcements" element={<OperationsProvider>
               <AnnouncementPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/notifications" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/notifications" element={<OperationsProvider>
               <OperationsNotificationsPage />
-            </OperationsProvider></ProtectedRoute>} />
-        <Route path="/operations/settings" element={<ProtectedRoute><OperationsProvider>
+            </OperationsProvider>} />
+        <Route path="/operations/settings" element={<OperationsProvider>
               <OperationsSettingsPage />
-            </OperationsProvider></ProtectedRoute>} />
-        {/* Catch-all: redirect unknown routes to login */}
-        <Route path="*" element={<Navigate to="/operations/login" replace />} />
+            </OperationsProvider>} />
       </Routes>
     </BrowserRouter>;
 }
